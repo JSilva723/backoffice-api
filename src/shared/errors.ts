4fa540bae5d353504cae4d211f592e3e1293c0bb
JSilva723 <silvajonatanivan@gmail.com>
2024-05-07 22:48:38 -0300

    feat: add 401 and 403 errors

--- src/shared/errors.ts
+++ src/shared/errors.ts
@@ -7,6 +7,42 @@ export abstract class HttpError extends Error {
     }
 }
 
+export class BadRequestError extends HttpError {
+    statusCode = 400
+    constructor(message: string) {
+        super(message)
+        this.name = 'BadRequestError'
+    }
+
+    public static drop(message: string) {
+        return new BadRequestError(message)
+    }
+}
+
+export class UnauthorizedError extends HttpError {
+    statusCode = 401
+    constructor(message: string) {
+        super(message)
+        this.name = 'UnauthorizedError'
+    }
+
+    public static drop(message: string) {
+        return new BadRequestError(message)
+    }
+}
+
+export class ForbiddenError extends HttpError {
+    statusCode = 403
+    constructor(message: string) {
+        super(message)
+        this.name = 'ForbiddenError'
+    }
+
+    public static drop(message: string) {
+        return new BadRequestError(message)
+    }
+}
+
 export class NotFoundError extends HttpError {
     statusCode = 404
     constructor(message: string) {
@@ -19,14 +55,3 @@ export class NotFoundError extends HttpError {
     }
 }
 
-export class BadRequestError extends HttpError {
-    statusCode = 400
-    constructor(message: string) {
-        super(message)
-        this.name = 'BadRequestError'
-    }
-
-    public static drop(message: string) {
-        return new BadRequestError(message)
-    }
-}
