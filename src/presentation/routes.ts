5426fae369add41daa61731f31daadc2a1036225
JSilva723 <silvajonatanivan@gmail.com>
2024-05-11 02:59:37 -0300

    refactor: remove verifytoken

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { User } from '@context/user/routes'
-import { errorHandler, verifyToken } from './middleware'
+import { errorHandler } from './middleware'
 import { Project } from '@context/project/routes'
 import { Tier } from '@context/tier/routes'
 
@@ -12,7 +12,7 @@ export class AppRoutes {
         })
         router.use(User.routes)
         router.use(Project.prefix, Project.routes)
-        router.use(Tier.prefix, verifyToken, Tier.routes)
+        router.use(Tier.prefix, Tier.routes)
         router.use(errorHandler)
 
         return router
